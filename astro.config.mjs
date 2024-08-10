3c8dabef51e1bdcae1c39befd62bd56f87fad5e3
null <null>
2024-08-10 10:44:07 +0900

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [UnoCSS({injectReset: true})]
 });
